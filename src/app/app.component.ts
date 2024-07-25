0d4c8363e59b9e05ddca1f9e05bb26ca25f7cea4
jonathan gallego <jonathan.gallego7@gmail.com>
2024-07-25 15:36:13 -0500

    fix: se organiza orientación de galeria fotos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PerfilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
